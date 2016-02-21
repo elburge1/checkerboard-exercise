38a8b0fbe3160d872f91cedd9f7532a917fac435
Edward Burgess <elburgess1@gmail.com>
2016-02-21 15:30:12 -0800

    add the rainbow checkerboard FUCK YEAH

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 // Your JS goes here
-
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 var body = document.getElementsByTagName('body')[0];
 for (var i = 0; i < 9; i++) {
   for (var j = 1; j <= 9; j++) {
-      var tile = document.createElement('div');
-      tile.style.height = '11.1%';
-      tile.style.width = '11.1%';
-      tile.style.float = 'left';
-      tile.style.paddingTop = '10%';
-      if ((j + i) % 2 === 0) {
-        tile.style.backgroundColor = 'white';
-      } else {
-        tile.style.backgroundColor = 'black';
-      }
-      body.appendChild(tile);
-    }
+    var tile = document.createElement('div');
+    tile.style.height = '11.1%';
+    tile.style.width = '11.1%';
+    tile.style.float = 'left';
+    tile.style.paddingTop = '10%';
+    tile.style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16);
+    body.appendChild(tile);
   }
+};

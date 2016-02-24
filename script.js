69cf5c2daf09dbe98e522b7acd3fce5c13a2aa80
Edward Burgess <elburgess1@gmail.com>
2016-02-24 11:49:10 -0800

    fix syntax errors

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 // Your JS goes here
-var body = document.getElementsByTagName('body')[0];
-for (var i = 0; i < 9; i++) {
-  for (var j = 1; j <= 9; j++) {
+(function() {
+  'use strict';
+
+  var body = document.getElementsByTagName('body')[0];
+
+  for (var i = 0; i < 9; i++) {
+    for (var j = 1; j <= 9; j++) {
       var tile = document.createElement('div');
+
       tile.style.height = '11.1%';
       tile.style.width = '11.1%';
       tile.style.float = 'left';
       tile.style.paddingTop = '10%';
       if ((j + i) % 2 === 0) {
         tile.style.backgroundColor = 'white';
-      } else {
+      }
+      else {
         tile.style.backgroundColor = 'black';
       }
       body.appendChild(tile);
     }
   }
+})();
